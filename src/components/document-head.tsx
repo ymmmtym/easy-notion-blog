4614709e67bfe9c420c87df3a8c413f2b8e25e7a
ymmmtym <26668352+ymmmtym@users.noreply.github.com>
2022-09-16 01:24:26 +0900

    chore: rename head to personal name

--- src/components/document-head.tsx
+++ src/components/document-head.tsx
@@ -3,9 +3,9 @@ import { useRouter } from 'next/router'
 
 import { NEXT_PUBLIC_URL } from '../lib/notion/server-constants'
 
-export const SITE_TITLE = 'easy-notion-blog'
+export const SITE_TITLE = "yumenomatayume's engineer blog"
 export const SITE_DESCRIPTION =
-  'Easy to start your blog. You can write on your Notion.'
+  "yumenomatayume's engineer blog"
 
 const DocumentHead = ({ title = '', description = '', urlOgImage = '' }) => {
   const { asPath } = useRouter()
